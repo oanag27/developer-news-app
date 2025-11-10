30942baead8f62834d502b4a552f003434ec630b
913-Gog-Oana <oana.gog@stud.ubbcluj.ro>
2025-11-10 23:28:42 +0200

    Add frontend articles fetching

--- frontend/project/src/pages/BrowsingPage.tsx
+++ frontend/project/src/pages/BrowsingPage.tsx
@@ -1,13 +1,39 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 
-export default function BrowsingPage() {
+export interface IBrowsingPage {
+  title: string;
+  url: string;
+  source: string;
+}
 
+export default function BrowsingPage() {
+  const [data, setData] = useState<IBrowsingPage[]>([]);
+  const fetchArticlesData = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/articles/fetch`
+      );
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error("Error fetching articles:", error);
+    }
+  };
+  useEffect(() => {
+    fetchArticlesData();
+  }, []);
   return (
     <div>
       <Navbar />
       <div className="container py-5">
-        <h2>Browse Articles</h2>
+        {data.map((article: IBrowsingPage, index) => (
+          <div key={index}>
+            <h2>{article.title}</h2>
+            <a href={article.url}>{article.url}</a>
+            <p>{article.source}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
